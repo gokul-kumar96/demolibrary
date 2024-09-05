8b2b6f8bb37482df34464ab3d42fe13bb94941b7
Dell <gokulkumar9664@gmal.com>
2024-09-05 14:40:46 +0530

    library module

--- devicelibrary/src/main/java/com/example/devicelibrary/DeviceActivity.kt
+++ devicelibrary/src/main/java/com/example/devicelibrary/DeviceActivity.kt
@@ -1,6 +1,8 @@
 package com.example.devicelibrary
 
 import android.os.Bundle
+import android.provider.Settings
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
@@ -9,10 +11,19 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class DeviceActivity : ComponentActivity() {
+    lateinit var txtDeviceId : TextView;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_device);
+        txtDeviceId = findViewById(R.id.txt_device_id) as TextView
+
+        // Fetching Android ID and storing it into a constant
+        val mId = Settings.Secure.getString(contentResolver, Settings.Secure.ANDROID_ID)
+        txtDeviceId.text = "Android Device Id : "+mId;
+        // Displaying the Android ID into the TextView
+
         //enableEdgeToEdge()
-        Toast.makeText(applicationContext,"Hello",Toast.LENGTH_SHORT).show();
+      //  Toast.makeText(applicationContext,"Hello",Toast.LENGTH_SHORT).show();
        // setContentView(R.layout.activity_device)
       /*  ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
